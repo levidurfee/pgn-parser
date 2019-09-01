c6be74da2fd86ededebea4d926accf276c9668fd
Levi Durfee <levi.durfee@gmail.com>
2019-09-01 09:50:57 -0400

    Get parsing working

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 
 const rules = require('./rules');
 
-module.exports = function parsePGN(pgn) {
-    let strippedPGN = pgn.replace((/  |\t|\r\n|\n|\r/gm),"");
+module.exports = function parsePGN(pgnString) {
+    const lastHeaderElement = pgnString.lastIndexOf("]\n\n") + 1;
+    const headerString = pgnString.substr(0, lastHeaderElement);
+    const historyString = pgnString.substr(lastHeaderElement);
+
+    //console.log(headerString, historyString);
+
+    let strippedPGN = historyString.replace((/  |\t|\r\n|\n|\r/gm), " ");
+    //console.log(strippedPGN);
     let parsedPGN = rules.parse(strippedPGN);
 
     return parsedPGN;
